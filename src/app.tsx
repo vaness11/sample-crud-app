07a41076d55bc487772e333ba4024a3f8e471318
Ivan G <nightwish1111@gmail.com>
2023-04-22 11:34:46 +0300

    basic template with vite & mui

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Layout } from './layout/layout';
+
+function App() {
+  return <Layout>TODO</Layout>;
+}
+
+export default App;
---
+++ src/layout/app-bar.tsx
@@ -0,0 +1,24 @@
+import { styled } from '@mui/material/styles';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import { DRAWER_WIDTH } from "./constants";
+
+type AppBarProps = MuiAppBarProps & {
+  open?: boolean;
+};
+export const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: DRAWER_WIDTH,
+    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
